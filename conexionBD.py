475a67961cbc09da70f1cf9cc13255ecc225b3ee
Edison-Insuasty <edisonins15@gmail.com>
2023-03-20 08:33:42 -0500

    Base

--- conexionBD.py
+++ conexionBD.py
@@ -4,10 +4,10 @@ import mysql.connector
 def connectionBD():
     try:
         mydb = mysql.connector.connect(
-            host ="edisoninsuasty.mysql.pythonanywhere-services.com",
-            user ="edisoninsuasty",
-            password ="matricula1234",
-            db = "edisoninsuasty$default"
+            host ="localhost",
+            user ="root",
+            password ="",
+            db = "bd_parrilla"
             )
     except Exception:
         abort(500)
